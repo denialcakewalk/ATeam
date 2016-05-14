ace6d3aebfc912b11b50ff54faa0c5a1095d4483
Nisarg Parikh <nisargparikh_dev@hotmail.com>
2016-05-15 01:05:51 +0530

    Change : Add person details.

---
+++ scripts/components/PersonDetailsModal.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Modal from 'react-bootstrap-modal';
+
+class PersonDetailsModal extends React.Component {
+    constructor() {
+        super();
+
+        return {view: {showModal: false}};
+    }
+
+    componentDidMount() {
+        $(this.getDOMNode()).modal('show');
+    }
+
+    render() {
+        return (
+            <div className="modal fade">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span
+                                aria-hidden="true">&times;</span></button>
+                            <h4 className="modal-title">Modal title</h4>
+                        </div>
+                        <div className="modal-body">
+                            <p>One fine body&hellip;</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+;
+
+PersonDetailsModal.propTypes = {
+    handleHideModal: React.PropTypes.func.isRequired
+}
+
+export default PersonDetailsModal;

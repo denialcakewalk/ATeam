808891e6d5bb44915e9791b1347f4a28f4a2b006
Nisarg Parikh <nisargparikh_dev@hotmail.com>
2016-05-15 16:17:59 +0530

    Add Visitor Out.

--- scripts/components/VisitorOut.js
+++ scripts/components/VisitorOut.js
@@ -1,18 +1,61 @@
 import React from 'react';
 
 class CheckOut extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            visitorCheckIns: []
+        }
+
+        var checkInState = this;
+        $.ajax({
+            async: true,
+            type: "GET",
+            url: "http://192.101.102.165:4030/checkoutlist",
+        }).done(function (data) {
+            if(data.length > 0)
+            {
+                debugger;
+                var VisitorLists = [];
+                var VisitorIDs = data.map(function (_data) {
+                    return _data.visitorid;
+                });
+                VisitorIDs.filter(function(VisitorId) {
+                    $.ajax({
+                       async:true,
+                        type:"GET",
+                        url:"http://192.101.102.165:4030/newvisitors/visitor/" + VisitorId,
+                    }).done(function (checkInData) {
+                        VisitorLists.push(checkInData);
+                    }).fail(function (xhr, status, err) {
+                        console.error(err);
+                    }).then(function (checkInData) {
+                        debugger;
+                    });
+                })
+
+                checkInState.setState({visitorCheckIns:VisitorLists});
+            }
+        }).fail(function (xhr, status, err) {
+            console.log(err);
+        });
+
+    }
+
+    searchVisitor() {
+
+    }
+
     render() {
-
-
-
         var imgHeightWidth = {
-            height:32,
-            width:56
+            height: 32,
+            width: 56
         }
 
 
         var mobileFont = {
-            fontSize:12
+            fontSize: 12
         }
 
 
@@ -30,18 +73,22 @@ class CheckOut extends React.Component {
                                     <div className="row">
                                         <div className="col-md-6">
                                             <form role="form">
-                                                <div className="form-group">
-                                                    <input className="form-control" placeholder="Search.."/>
+
+                                                <div id="txtMobileDiv" className="form-group">
+                                                    <div className="input-group">
+                                                        <input id="txtVisitorSearch" type="number"
+                                                               className="form-control"
+                                                               placeholder="Visitor Name or Contact Number"/>
+                                                     <span className="form-group input-group-btn">
+                                                        <button className="btn btn-primary" type="button"
+                                                                onClick={this.searchVisitor}>Search
+                                                        </button>
+                                                      </span>
+                                                    </div>
                                                 </div>
 
 
-
-
-
-
-
-
-                                             </form>
+                                            </form>
 
                                         </div>
 
@@ -56,9 +103,6 @@ class CheckOut extends React.Component {
                                                     <div className="col-xs-6">
 
 
-
-
-
                                                     </div>
                                                     <div className="col-xs-3">
 
@@ -82,22 +126,20 @@ class CheckOut extends React.Component {
                                                 </div>
 
 
-
-
-
-
                                             </form>
                                         </div>
 
 
                                     </div>
 
+
                                     <div className="row">
 
                                         <div className="col-md-2 col-xs-3">
                                             <form role="form">
                                                 <div className="form-group">
-                                                    <img className="form-control" src="./css/images/default.jpg" className="img-responsive" style={imgHeightWidth}/>
+                                                    <img className="form-control" src="./css/images/default.jpg"
+                                                         className="img-responsive" style={imgHeightWidth}/>
 
                                                 </div>
                                             </form>
@@ -123,7 +165,7 @@ class CheckOut extends React.Component {
 
                                     </div>
                                     <hr/>
-                                    
+
 
                                 </div>
                             </div>
@@ -132,7 +174,6 @@ class CheckOut extends React.Component {
 
 
                     </div>
-
 
 
                 </div>
